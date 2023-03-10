c43886730db30ed88047f0f6f80bb7d8d93ade5b
Thomas A. Ciarfuglia <thomas.ciarfuglia@gmail.com>
2023-03-10 08:46:08 +0100

    Corrected a small typo in equalization test

--- src/test/test1.cpp
+++ src/test/test1.cpp
@@ -182,12 +182,12 @@ void test_bilateral() {
 void test_equalization() {
     Image im = load_image("data/dog.jpg");
     Image eqim1 = histogram_equalization_rgb(im, 256);
-    save_png(eqim1, "output/equalized_hsv");
+    save_png(eqim1, "output/equalized_rgb");
     Image gt1 = load_image("data/equalized_rgb.png");
     TEST(same_image(eqim1, gt1));
 
     Image eqim2 = histogram_equalization_hsv(im, 256);
-    save_png(eqim2, "output/equalized_rgb");
+    save_png(eqim2, "output/equalized_hsv");
     Image gt2 = load_image("data/equalized_hsv.png");
     TEST(same_image(eqim2, gt2));
 }
